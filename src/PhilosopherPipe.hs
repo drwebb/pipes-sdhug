fbd183f55bc1703968cae21e331c575da84940a3
Tristan Webb <t2webb@ucsd.edu>
2014-06-20 13:38:06 -0700

    Removed lenses, too big

--- src/PhilosopherPipe.hs
+++ src/PhilosopherPipe.hs
@@ -1,13 +1,11 @@
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE TemplateHaskell           #-}
 
 module PhilosopherPipe where
 
 import           Control.Concurrent     (threadDelay)
 import           Control.Concurrent.Async
 import           Control.Concurrent.STM
-import           Control.Lens
 import           Control.Monad
 import           Pipes
 import           Pipes.Concurrent
@@ -16,17 +14,15 @@ import           System.Random.MWC
 
 data ThreadState = Think | Eat deriving Show
 
-data Philosopher = Philosopher { _name  :: String
-                               , _rfork :: TVar Bool
-                               , _lfork :: TVar Bool
+data Philosopher = Philosopher { name  :: String
+                               , rfork :: TVar Bool
+                               , lfork :: TVar Bool
                                }
-makeLenses ''Philosopher
 
-data Chair = Chair { _phil :: Maybe Philosopher
-                   , _inbox :: Input Philosopher
-                   , _outbox :: Output Philosopher 
+data Chair = Chair { phil :: Maybe Philosopher
+                   , inbox :: Input Philosopher
+                   , outbox :: Output Philosopher
                    }
-makeLenses ''Chair
 
 type Fork = TVar Bool
 
@@ -39,15 +35,15 @@ randomDelay =
      threadDelay $ floor (500000 * vs)
 
 takeFork :: Fork -> STM ()
-takeFork fork = do forkFree <- readTVar fork 
+takeFork fork = do forkFree <- readTVar fork
                    if forkFree then writeTVar fork False
-                   else retry 
+                   else retry
 
-releaseFork :: Fork -> STM () 
+releaseFork :: Fork -> STM ()
 releaseFork fork = writeTVar fork True
 
 dinePipe :: MonadIO m => Pipe Philosopher (Philosopher, ThreadState) m ()
-dinePipe = 
+dinePipe =
   do ph <- await
      forever $ do yield (ph, Think)
                   liftIO randomDelay
@@ -57,38 +53,38 @@ dinePipe =
                   finishEating ph
 
 startEating :: MonadIO m => Philosopher -> m ()
-startEating ph = liftIO . atomically $ do takeFork (ph^.rfork) 
-                                          takeFork (ph^.lfork)
+startEating ph = liftIO . atomically $ do takeFork (rfork ph)
+                                          takeFork (lfork ph)
 
 finishEating :: MonadIO m => Philosopher -> m ()
-finishEating ph = liftIO . atomically $ do releaseFork (ph^.rfork)
-                                           releaseFork (ph^.lfork)
+finishEating ph = liftIO . atomically $ do releaseFork (rfork ph)
+                                           releaseFork (lfork ph)
 
 pipeline :: Chair -> IO (Async ())
-pipeline chair = 
+pipeline chair =
   async $ do liftIO $ print "Pipeline started"
-             runEffect $ fromInput (chair^.inbox) >-> 
-                         dinePipe >-> 
-                         P.map (\(p,ts) -> (p^.name, ts)) >-> P.print
-             performGC           
-
-sitDown :: Chair -> Philosopher -> IO (Async ())
-sitDown chair ph =  
-  async $ do runEffect $ yield ph >-> toOutput (chair^.outbox)
-             liftIO . print $ (ph^.name) ++ " sits down to eat."
+             runEffect $ fromInput (inbox chair) >->
+                         dinePipe >->
+                         P.map (\(p,ts) -> (name p, ts)) >-> P.print
              performGC
 
-run :: IO ()   
+sitDown :: Chair -> Philosopher -> IO (Async ())
+sitDown chair ph =
+  async $ do runEffect $ yield ph >-> toOutput (outbox chair)
+             liftIO . print $ (name ph) ++ " sits down to eat."
+             performGC
+
+run :: IO ()
 run = do let n = length philosopherNames
          forks <- replicateM n (newTVarIO True)
          (output, input) <- spawn Unbounded
          let philosophers = flip map [0..n-1] $ \i ->
                Philosopher (philosopherNames !! i)
                            (forks !! i)
-                           (forks !! ((i+1) `mod` n)) 
+                           (forks !! ((i+1) `mod` n))
          let chairs = replicate n (Chair Nothing input output)
          zipWithM_ sitDown chairs philosophers
-         as <- forM chairs $ \c -> pipeline c 
+         as <- forM chairs $ \c -> pipeline c
          replicateM_ 15 randomDelay
          mapM_ cancel as
          print "Dinner is over"
